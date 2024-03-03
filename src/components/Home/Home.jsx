04033269e66b4604c56c26656bb682448992f3bc
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-03-03 19:54:38 +0500

    improved stability

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,12 +4,30 @@ const Home = () => {
     <div className="home">
       <div className="wrapper">
         <div className="left">
-          <h5>NATIVE</h5>
+          <h5>HELLO $GUEST</h5>
           <p>
             I’m MuhammadYusuf. <br /> Software Engineer from Central Asia.
           </p>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <div className="text">
+            <div className="inner-text">
+              <p>
+                I’m currently studying <span className="php">PHP</span> and
+                <span className="js"> JavaScript</span> <br /> Still, feel free
+                to reach out to see if we can
+                <br /> collaborate in some capacity.
+              </p>
+            </div>
+          </div>
+          <div className="second">
+            <div className="services">
+              <h6>SERVICES</h6>
+              <p>Web Development</p>
+            </div>
+            <div className="contact"></div>
+          </div>
+        </div>
       </div>
       <div className="footer"></div>
     </div>
