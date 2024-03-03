016de87364cff6c164c73d11d7e4a08224c89571
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-03-03 19:01:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Home from "./components/Home/Home";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="wrapper">
+        <div className="left">
+          <h5>NATIVE</h5>
+          <p>
+            I’m MuhammadYusuf. <br /> Software Engineer from Central Asia.
+          </p>
+        </div>
+        <div className="right"></div>
+      </div>
+      <div className="footer"></div>
+    </div>
+  );
+};
+
+export default Home;
