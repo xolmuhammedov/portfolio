57e3f7051dc1dfccc6414eeef00c8ccb0f82525f
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-03-04 17:04:10 +0500

    another changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,15 +21,21 @@ const Home = () => {
             </div>
           </div>
           <div className="second">
-            <div className="services">
-              <h6>SERVICES</h6>
-              <p>Web Development</p>
+            <div className="second-inner">
+              <p>
+                I am trying to learn new things everyday i also want to have
+                more friends from foreign countries, I am a good friend with
+                anyone <span id="contact">contact me.</span>
+              </p>
             </div>
-            <div className="contact"></div>
           </div>
         </div>
       </div>
-      <div className="footer"></div>
+      <div className="footer">
+        <div className="text">
+          <h1 id="happines">Happines is in simpilicity</h1>
+        </div>
+      </div>
     </div>
   );
 };
